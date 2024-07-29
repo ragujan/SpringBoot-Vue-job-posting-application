e28a56a0e6f39879bb126e96a5163ab5b83128f8
Ragujan Rag JN <stiflerwedontgiveup@gmail.com>
2024-07-29 12:25:52 +0530

    feat: inital commit

---
+++ src/main/java/com/rag/RagsJobPosts/HelloController.java
@@ -0,0 +1,16 @@
+package com.rag.RagsJobPosts;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@GetMapping("/")
+	public String index(Model model) {
+		return "hey";
+	}
+
+}
---
+++ src/main/java/com/rag/RagsJobPosts/RagsJobPostsApplication.java
@@ -0,0 +1,13 @@
+package com.rag.RagsJobPosts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagsJobPostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagsJobPostsApplication.class, args);
+	}
+
+}
